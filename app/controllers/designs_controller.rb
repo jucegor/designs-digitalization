e6dd0f8e25423330f7c895490ca8761bdcd879ef
Julio Gonzalez <jucegor@gmail.com>
2022-06-28 19:33:32 -0500

    Better looking on designs index

--- app/controllers/designs_controller.rb
+++ app/controllers/designs_controller.rb
@@ -10,7 +10,8 @@ class DesignsController < ApplicationController
     when 'engineer'
       @designs = policy_scope(Design.where(user: current_user, status: 'activo'))
     end
-    params[:query].present? ? (@designs = Design.where("project_number ILIKE ?", "%#{params[:query]}%")) : (@designs = Design.all)
+
+    params[:query].present? ? (@designs = Design.where("project_number LIKE '%#{params[:query]}%' OR project_name LIKE '%#{params[:query].upcase}%'")) : (@designs = Design.all)
   end
 
   def all_designs
